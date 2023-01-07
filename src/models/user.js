0af34875dbc03a3c3b91a42b9d0633be474986b0
kanavtec11 <122120258+kanavtec11@users.noreply.github.com>
2023-01-07 16:43:58 +0530

    Update user.js
    user model updated with timestamp

--- src/models/user.js
+++ src/models/user.js
@@ -14,6 +14,6 @@ const UserSchema = mongoose.Schema({
         type: String,
         required: true
     }
-}, { timestamp: true });
+}, { timestamps: true });
 
 module.exports = mongoose.model("User", UserSchema);
